0880d84e31158ca83e2dccea67c9f748617e3953
unknown <likhitha.aramalenahalli.vijayagowda@tuhh.de>
2026-04-15 12:32:07 +0200

    Fix: Add mock AVR headers for compilation on Linux - prioritize local includes

---
+++ test/support/avr/io.h
@@ -0,0 +1,14 @@
+/**
+ * @file avr/io.h
+ * @brief Mock AVR I/O header for testing on native platform
+ * 
+ * This file is a mock replacement for the real avr/io.h header.
+ * It allows AVR driver code to compile on x86/x64 systems for unit testing.
+ */
+
+#ifndef AVR_IO_H_
+#define AVR_IO_H_
+
+#include "mock_avr_io.h"
+
+#endif /* AVR_IO_H_ */
